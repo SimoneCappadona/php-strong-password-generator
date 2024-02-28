9e0e7e69a17e32448a9e35a561a08c50c2a6ccd6
SimoneCappadona <simocappa95@gmail.com>
2024-02-28 16:49:06 +0100

    sessiona start

--- php/function.php
+++ php/function.php
@@ -1,7 +1,30 @@
-<?php 
+<?php
+// all the letters of the alphabet in lowercase
+$textLowercase = 'abcdefghijklmnopqrstuvwxyz';
+// all letters of the alphabet in uppercase
+$textUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// all numbers
+$numbers = '0123456789';
+// all lettere symbol
+$specialtext = '!@#$%&?';
 
 
+$validChars = $textLowercase . $textUppercase . $numbers . $specialtext;
+$newPassword = '';
 
+// General function waiting to be called with the right parameters
+function generatePassword($characters, $length)
+{
+    $password = '';
+    $charLength = strlen($characters);
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $characters[rand(0, $charLength - 1)];
+    }
+    return $password;
+}
 
-
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['password_length'])) {
+    $passwordLength = $_GET['password_length'];
+    $newPassword = generatePassword($validChars, $passwordLength);
+}
 ?>
