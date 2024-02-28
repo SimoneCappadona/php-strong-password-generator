61192463c5703106b57b44f6326e4182df04a782
SimoneCappadona <simocappa95@gmail.com>
2024-02-28 13:34:59 +0100

    add style

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+        <!-- my-css -->
+    <link rel="stylesheet" href="./style/style.css">
 </head>
+
 <body>
-    
+<header>
+        <div class="container mt-5 text-center">
+            <h1 class="text-success">Strong Password Generator</h1>
+        </div>
+    </header>
+
+    <main>
+        <div>
+
+        </div>
+    </main>
 </body>
+
 </html>
